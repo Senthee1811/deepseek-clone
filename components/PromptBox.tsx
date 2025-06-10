e3c7a3138da0986753180bf03f1c9113e38ac393
Senthee1811 <esentheepan@gmail.com>
2025-06-10 11:29:42 +0530

    Added Clerk

--- components/PromptBox.tsx
+++ components/PromptBox.tsx
@@ -21,7 +21,7 @@ const PromptBox = ({isLoading,setIsLoading}) => {
      />
 
      <div className='flex items-center justify-between text-sm'>
-        <div className='flex items-centert gap-2'>
+        <div className='flex items-center gap-2'>
             <p className='flex items-center gap-2 text-xs border border-gray-300/40 px-2 py-1
             rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
                 <Image className='h-5' src={assets.deepthink_icon} alt=""/>
